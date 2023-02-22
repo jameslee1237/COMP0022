4f5d0d1eb0c37b1c4936300b14c94fab99dd0f6d
jhtkoo0426 <jhtkoo@gmail.com>
2023-02-22 12:38:42 +0000

    Updated app.py to support creation of multiple tables

--- python/app.py
+++ python/app.py
@@ -71,55 +71,62 @@ class App:
     
     def create_table_with_data(self):
         cursor = self.cnx2.cursor()
-        cursor.execute("select database();")
-        record = cursor.fetchone()
-        print("connected to: ", record)
+        # cursor.execute("select database();")
+        # record = cursor.fetchone()
+        # print("connected to: ", record)
         cursor.execute("show tables;")
         r2 = cursor.fetchall()
+        print(r2, flush=True)
 
-        if r2 is not None:
-            cursor.execute("DROP TABLE IF EXISTS movies_ratings;")
-            self.TABLES['movies_ratings'] = (
-                "CREATE TABLE movies_ratings ("
-                "  user_ID INT NOT NULL,"
-                "  movie_ID INT NOT NULL,"
-                "  rating VARCHAR(255) NOT NULL,"
-                "  timestamp VARCHAR(255) NOT NULL);"
-            )
 
-            cursor.execute(self.TABLES['movies_ratings'])
-            
-            for i, row in self.ratings_data.iterrows():
-                if i == 0:
-                    print("INSERTING RECORDS")
-                sql = "INSERT INTO movies.movies_ratings VALUES (%s, %s, %s, %s)"
-                cursor.execute(sql, tuple(row))
-                self.cnx2.commit()
-            cursor.close()
-            return 0
-        else:
-            # Create SQL for creating tables
-            cursor.execute("DROP TABLE IF EXISTS movies_data;")
-            self.TABLES['movies_data'] = (
-                "CREATE TABLE movies_data ("
-                "  movie_ID INT NOT NULL AUTO_INCREMENT,"
-                "  title VARCHAR(255) NOT NULL,"
-                "  genre VARCHAR(255) NOT NULL,"
-                "  PRIMARY KEY(movie_ID));"
-            )
+        # Create SQL for creating tables
+        cursor.execute("DROP TABLE IF EXISTS movies_data;")
+        cursor.execute("DROP TABLE IF EXISTS movies_ratings;")
 
-            cursor.execute(self.TABLES['movies_data'])
+        self.TABLES['movies_data'] = (
+            "CREATE TABLE movies_data ("
+            "  movie_ID INT NOT NULL AUTO_INCREMENT,"
+            "  title VARCHAR(255) NOT NULL,"
+            "  genre VARCHAR(255) NOT NULL,"
+            "  PRIMARY KEY(movie_ID));"
+        )
+        self.TABLES['movies_ratings'] = (
+            "CREATE TABLE movies_ratings ("
+            "  user_ID INT NOT NULL,"
+            "  movie_ID INT NOT NULL,"
+            "  rating VARCHAR(255) NOT NULL,"
+            "  timestamp VARCHAR(255) NOT NULL);"
+        )
 
-            # Populate tables
-            for i, row in self.pdata.iterrows():
-                if i == 0:
-                    print("INSERTING RECORDS")
-                sql = "INSERT INTO movies.movies_data VALUES (%s, %s, %s)"
-                cursor.execute(sql, tuple(row))
-                self.cnx2.commit()
+        for table_name in self.TABLES:
+            table_description = self.TABLES[table_name]
+            try:
+                print("Creating table {}: ".format(table_name), end='', flush=True)
+                cursor.execute(table_description)
+            except mysql.connector.Error as err:
+                if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                    print("already exists.", flush=True)
+                else:
+                    print(err.msg, flush=True)
+            else:
+                print("OK", flush=True)
 
-            
-            cursor.close()
+        # Populate tables
+        for i, row in self.pdata.iterrows():
+            if i == 0:
+                print("INSERTING RECORDS", flush=True)
+            sql = "INSERT INTO movies.movies_data VALUES (%s, %s, %s)"
+            cursor.execute(sql, tuple(row))
+            self.cnx2.commit()
+        
+        for i, row in self.ratings_data.iterrows():
+            if i == 0:
+                print("INSERTING RECORDS", flush=True)
+            sql = "INSERT INTO movies.movies_ratings VALUES (%s, %s, %s, %s)"
+            cursor.execute(sql, tuple(row))
+            self.cnx2.commit()
+
+        cursor.close()
 
     def print_first_10_terminal(self):
         result = self.print_first_10()
